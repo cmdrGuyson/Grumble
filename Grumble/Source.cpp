331ff47b35ed7f60e42dbe4ff3bda92522cc8776
cmdrGuyson <gayangakuruppu@gmail.com>
2019-08-10 10:27:28 +0530

    initial-setup

---
+++ Grumble/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void menu();
+
+#define logln(x) cout << x << endl;
+#define log(x) cout << x;
+
+int main() {
+
+	menu();
+
+}
+
+void menu() {
+
+	logln("\n\t\t\t=======================================");
+	logln("\n\t\t\t  I-CALL COMPLAINT MANANGEMENT SYSTEM  ");
+	logln("\n\t\t\t=======================================");
+}
